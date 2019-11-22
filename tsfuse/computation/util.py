2af582e22865fcb5552b26ee0b72daed01ceb771
Arne De Brabandere <arne.debrabandere@cs.kuleuven.be>
2019-11-22 12:42:02 +0100

    Column names of constructed features

--- tsfuse/computation/util.py
+++ tsfuse/computation/util.py
@@ -8,17 +8,18 @@ def to_dataframe(result):
         import pandas as pd
 
         def collection_to_dataframe(n, x):
+            n = str(n).replace('[', '(').replace(']', ')')
             df = pd.DataFrame()
             values = x.values
             if x.shape[1] > 1:
                 for d in range(x.shape[2]):
                     for t in range(x.shape[1]):
-                        df['{}[{}, {}]'.format(str(n), t, d)] = values[:, t, d]
+                        df['{}[{}, {}]'.format(n, t, d).replace('[', '{').replace(']', '}')] = values[:, t, d]
             elif x.shape[2] > 1:
                 for d in range(x.shape[2]):
-                    df['{}[{}]'.format(str(n), d)] = values[:, 0, d]
+                    df['{}[{}]'.format(n, d).replace('[', '{').replace(']', '}')] = values[:, 0, d]
             else:
-                df[str(n)] = values[:, 0, 0]
+                df[n] = values[:, 0, 0]
             return df
 
         assert len(set(len(result[node]) for node in result if result[node] is not None)) <= 1
@@ -27,9 +28,9 @@ def to_dataframe(result):
         for node in result:
             output = result[node]
             if output is not None:
-                if isinstance(output.shape[1], (tuple, list, np.ndarray)):
-                    for i in range(len(output.shape[1])):
-                        dfs.append(collection_to_dataframe(node, output.values[i]))
+                if isinstance(output, (list, np.ndarray)):
+                    for i in range(len(output)):
+                        dfs.append(collection_to_dataframe(node, output[i]))
                 else:
                     dfs.append(collection_to_dataframe(node, output))
 
