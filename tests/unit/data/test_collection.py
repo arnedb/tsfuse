98a66c66f525c2e7f94fd487cf6dafe0ff8212ad
Arne De Brabandere <arne.debrabandere@cs.kuleuven.be>
2020-09-18 17:16:46 +0200

    Converting collections from/to data frames

--- tests/unit/data/test_collection.py
+++ tests/unit/data/test_collection.py
@@ -1,6 +1,8 @@
 from tsfuse.data import Collection
 
 import numpy as np
+import pandas as pd
+
 from tsfuse.data import Type, Tags
 from tsfuse.data.tags import quantity, body_part
 from tsfuse.transformers import Ratio
@@ -71,3 +73,119 @@ def test_tag_propagation():
     assert Ratio().transform(y, z).tags.body_part == 'right'
     assert quantity not in Ratio().transform(x, z).tags
     assert body_part not in Ratio().transform(x, z).tags
+
+
+def test_from_dataframe_time_series_fixed_length():
+    df = pd.DataFrame([
+        ['A', 1, 10, 20, 30],
+        ['A', 2, 11, 21, 31],
+        ['A', 3, 12, 22, 33],
+        ['B', 1, 40, 50, 60],
+        ['B', 2, 41, 51, 61],
+        ['B', 3, 42, 52, 63],
+    ], columns=['id', 'time', 'x', 'y', 'z'])
+    collection = Collection.from_dataframe(df)
+    assert collection.values.shape == (2, 3, 3)
+    np.testing.assert_almost_equal(collection.values, [
+        [[10, 20, 30],
+         [11, 21, 31],
+         [12, 22, 33],],
+        [[40, 50, 60],
+         [41, 51, 61],
+         [42, 52, 63],],
+    ])
+    np.testing.assert_equal(collection.id, ['A', 'B'])
+
+
+def test_to_dataframe_time_series_fixed_length():
+    collection = Collection([
+        [[10, 20, 30],
+         [11, 21, 31],
+         [12, 22, 33],],
+        [[40, 50, 60],
+         [41, 51, 61],
+         [42, 52, 63],],
+    ], index=[
+        [0, 1, 2],
+        [0, 1, 2],
+    ], dimensions=['x', 'y', 'z'])
+    df = collection.to_dataframe(column_id='id', column_sort='time')
+    np.testing.assert_equal(df.columns.values, ['id', 'time', 'x', 'y', 'z'])
+    np.testing.assert_equal(df.values, [
+        [0, 0, 10, 20, 30],
+        [0, 1, 11, 21, 31],
+        [0, 2, 12, 22, 33],
+        [1, 0, 40, 50, 60],
+        [1, 1, 41, 51, 61],
+        [1, 2, 42, 52, 63],
+    ])
+
+
+def test_from_dataframe_time_series_variable_length():
+    df = pd.DataFrame([
+        ['A', 1, 10, 20, 30],
+        ['A', 2, 11, 21, 31],
+        ['A', 3, 12, 22, 33],
+        ['B', 1, 40, 50, 60],
+        ['B', 2, 41, 51, 61],
+    ], columns=['id', 'time', 'x', 'y', 'z'])
+    collection = Collection.from_dataframe(df)
+    np.testing.assert_almost_equal(collection.values[0].values, [
+        [[10, 20, 30],
+         [11, 21, 31],
+         [12, 22, 33],],
+    ])
+    np.testing.assert_almost_equal(collection.values[1].values, [
+        [[40, 50, 60],
+         [41, 51, 61],],
+    ])
+    np.testing.assert_equal(collection.id, [['A'], ['B']])
+
+
+def test_to_dataframe_time_series_variable_length():
+    collection = Collection([
+        [[10, 20, 30],
+         [11, 21, 31],
+         [12, 22, 33],],
+        [[40, 50, 60],
+         [41, 51, 61],],
+    ], index=[
+        [0, 1, 2],
+        [0, 1],
+    ], dimensions=['x', 'y', 'z'])
+    df = collection.to_dataframe()
+    np.testing.assert_equal(df.columns.values, ['id', 'time', 'x', 'y', 'z'])
+    np.testing.assert_equal(df.values, [
+        [0, 0, 10, 20, 30],
+        [0, 1, 11, 21, 31],
+        [0, 2, 12, 22, 33],
+        [1, 0, 40, 50, 60],
+        [1, 1, 41, 51, 61],
+    ])
+    
+
+def test_from_dataframe_attributes():
+    df = pd.DataFrame([
+        ['A', 10, 20, 30],
+        ['B', 40, 50, 60],
+    ], columns=['id', 'x', 'y', 'z'])
+    collection = Collection.from_dataframe(df)
+    assert collection.values.shape == (2, 1, 3)
+    np.testing.assert_almost_equal(collection.values, [
+        [[10, 20, 30]],
+        [[40, 50, 60]],
+    ])
+    np.testing.assert_equal(collection.id, ['A', 'B'])
+
+
+def test_to_dataframe_attributes():
+    collection = Collection([
+        [[10, 20, 30]],
+        [[40, 50, 60]],
+    ], dimensions=['x', 'y', 'z'])
+    df = collection.to_dataframe()
+    np.testing.assert_equal(df.columns.values, ['id', 'x', 'y', 'z'])
+    np.testing.assert_equal(df.values, [
+        [0, 10, 20, 30],
+        [1, 40, 50, 60],
+    ])
--- tsfuse/data/__init__.py
+++ tsfuse/data/__init__.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 
 import numpy as np
@@ -13,38 +14,139 @@ __all__ = [
 
 class Collection(object):
     """
-    Data collection for representing time series and attributes.
+    Data structure for representing time series data and attribute-value data.
+
+    Internally, collections store data as a three-dimensional arrays of shape
+    `(N, t, d)` where:
+
+    - `N` is the number of instances,
+    - `t` is the number of time stamps per instance,
+    - `d` is the number of dimensions.
+
+    **Representing time series data**
+
+    For time series data, each instance represents a window of `t` time stamps, 
+    each having `d` values, i.e., one value per dimension. Hence, each window
+    is a multivariate time series with `d` variables and `t` samples per 
+    variable.
+
+    Note that the above representation requires a fixed window size `t` for all
+    instances. Variable size windows can be represented in two ways:
+
+    - Pad each window to the maximal length using a value `x` and set 
+      ``mask_value=x``.
+    - If the above is not desired due to memory issues (since padding
+      increases the number of values that need to be stored), create one
+      collection for each window (with `N = 1`) and initialize a new collection
+      with a list of these collections instead of a three-dimensional array.
+
+    **Representing attribute-value data**
+
+    Attribute-value data is represented using the same three-dimensional array.
+    However, `t = 1` as attributes have no time dimension.
+
+    Examples
+    --------
+
+    The examples below show how to create different kinds of collections.
+    See below for more advanced parameters to add instance identifiers, time 
+    stamps, dimension names, tags and units.
+
+    .. note::
+    
+        For convenience, it is also possible to convert a DataFrame to a 
+        Collection and vice versa using the :func:`Collection.from_dataframe` 
+        and :func:`~Collection.to_dataframe` methods.
+
+    Example 1: Create a time series collection with a fixed window size
+
+    .. code:: python
+       
+        # N=2 instances with t=5 time stamps and d=3 dimensions
+        fixed_window_time_series_collection = Collection([
+            # Instance A
+            [[1, 5, 10],   # Time stamp 1
+             [2, 4, 20],   # Time stamp 2
+             [3, 3, 30],   # Time stamp 3
+             [4, 2, 40],   # Time stamp 4
+             [5, 1, 50]],  # Time stamp 5
+            # Instance B
+            [[2, 16, 60],  # Time stamp 1
+             [4, 14, 70],  # Time stamp 2
+             [6, 12, 40],  # Time stamp 3
+             [8, 10, 50],  # Time stamp 4
+             [10, 8, 20]], # Time stamp 5
+        ])
+
+    Example 2: Create a time series collection with a variable window size
+    
+    .. code:: python
+       
+        # N=2 instances with d=3 dimensions:
+        # one with t=5 time stamps and one with t=4 time stamps
+
+        # Instance A
+        a = Collection([
+            [[1, 5, 10],   # Time stamp 1
+             [2, 4, 20],   # Time stamp 2
+             [3, 3, 30],   # Time stamp 3
+             [4, 2, 40],   # Time stamp 4
+             [5, 1, 50]]   # Time stamp 5
+        ])
+
+        # Instance B
+        b = Collection([
+            [[2, 16, 60],  # Time stamp 1
+             [4, 14, 70],  # Time stamp 2
+             [6, 12, 40],  # Time stamp 3
+             [8, 10, 50]]  # Time stamp 4
+        ])
+
+        # One collection with both instances
+        variable_window_time_series_collection = Collection([a, b])
+
+    Example 3: Create an attribute collection
+    
+    .. code:: python
+       
+        # N=2 instances with d=3 dimensions
+        attribute_collection = Collection([
+            # Instance A
+            [[1, 5, 10]],
+            # Instance B
+            [[8, 3, 40]],
+        ])
 
     Parameters
     ----------
 
     values : array-like
-        Three-dimensional array of shape ``(N, t, d)`` where:
-
-        - `N` is the number of windows
-        - `t` is the number of time stamps in each window
-        - `d` is the number of dimensions.
+        Three-dimensional array of shape `(N, t, d)`.
 
     index : array-like, optional
-        Two-dimensional array of shape ``(N, t)`` with the time stamps of each window.
+        Two-dimensional array of shape `(N, t)` with the time stamps of each 
+        window.
+
+    id : array-like, optional
+        One-dimensional array of length `N` with the instance identifiers.
 
     dimensions : array-like, optional
-        One-dimensional array of length ``d`` with the dimension names.
+        One-dimensional array of length `d` with the dimension names.
+
+    tags : Tags, optional
+        Tags specified by keys and values.
+
+    unit : optional
+        Unit, see the `Pint documentation <https://pint.readthedocs.io>`_.
 
     mask_value : optional
         Placeholder representing missing values.
         Can be used for representing variable-length time series.
         Default: ``nan``
-
-    unit : optional
-        Unit, see the `Pint documentation <https://pint.readthedocs.io>`_.
-
-    tags : optional
-        :class:`~tsfuse.data.tags.Tags` object which specifies values for one or more tag keys.
     """
 
-    def __init__(self, values, index=None, dimensions=None,
-                 mask_value=np.nan, unit=None, tags=None):
+    def __init__(self, values, index=None, id=None, dimensions=None,
+                 tags=None, unit=None, mask_value=np.nan):
         # Unit
         self._unit = unit
         # Tags
@@ -58,9 +160,11 @@ class Collection(object):
                     collections.append(values[i])
                 else:
                     collection_index = index[i] if index is not None else None
+                    collection_id = id[i] if id is not None else i
                     collections.append(Collection(
                         values[i],
                         index=collection_index,
+                        id=collection_id,
                         dimensions=dimensions,
                         mask_value=mask_value,
                         unit=self._unit,
@@ -68,9 +172,13 @@ class Collection(object):
                     ))
             self._values = np.array(collections)
             if index is None:
-                self._index = np.arange(np.sum([c.shape[0] for c in collections]))
+                self._index = [c.index for c in collections]
             else:
                 self._index = index
+            if id is None:
+                self._id = [c.id for c in collections]
+            else:
+                self._id = np.array(id, copy=False)
             if dimensions is None:
                 if len(set(tuple(c.dimensions) for c in collections)) == 1:
                     self._dimensions = collections[0].dimensions
@@ -81,8 +189,10 @@ class Collection(object):
         else:
             values = np.array(values, copy=False)
             index = np.array(index, copy=False) if index is not None else None
+            id = np.array(id, copy=False) if id is not None else None
             dimensions = np.array(dimensions, copy=False) if dimensions is not None else None
-            values, index, dimensions = _reshape(values, index, dimensions)
+            values, index, id, dimensions = \
+                _reshape(values, index, id, dimensions)
             # Determine data type of values
             self._dtype = values.dtype
             if np.issubdtype(self._dtype, np.number):
@@ -101,6 +211,14 @@ class Collection(object):
                 self._itype = np.int64
             else:
                 self._itype = object
+            # Determine type of id
+            self._idtype = id.dtype
+            if np.issubdtype(self._idtype, np.character):
+                self._idtype = np.str_
+            elif np.issubdtype(self._idtype, np.integer):
+                self._idtype = np.int64
+            else:
+                self._idtype = object
             # Determine type of dimensions
             self._ntype = dimensions.dtype
             if np.issubdtype(self._ntype, np.character):
@@ -112,8 +230,9 @@ class Collection(object):
             # Values
             self._values = np.empty(values.shape, dtype=self._dtype)
             self._values[:, :, :] = values
-            # Index and dimensions
+            # Index, id and dimensions
             self._index = np.array(index, copy=True, dtype=self._itype)
+            self._id = np.array(id, copy=True, dtype=self._idtype)
             self._dimensions = np.array(dimensions, copy=True, dtype=self._ntype)
         # Mask value
         self._mask_value = mask_value
@@ -131,6 +250,13 @@ class Collection(object):
         numpy.array : Two-dimensional array of shape ``(N, t)``
         """
         return self._index
+
+    @property
+    def id(self):
+        """
+        numpy.array : One-dimensional array of length ``N``
+        """
+        return self._id
 
     @property
     def dimensions(self):
@@ -232,12 +358,145 @@ class Collection(object):
         m = self.mask_value
         return Collection(values, dimensions=dimensions, index=index, mask_value=m)
 
+    def to_dataframe(self, column_id='id', column_sort='time'):
+        """
+        Convert to a DataFrame.
+        """
+        import pandas as pd
+        # Variable length time series collection
+        if isinstance(self.shape[1], tuple):
+            dataframes = []
+            for c in self.values:
+                df = pd.DataFrame(c.values.reshape((-1, c.shape[-1])))
+                df.columns = c.dimensions
+                df.insert(0, column_id, np.repeat(c.id, c.shape[1]))
+                df.insert(1, column_sort, c.index.flatten())
+                dataframes.append(df)
+            return pd.concat(dataframes, axis=0, ignore_index=True)
+        # Fixed length time series collection
+        elif self.shape[1] > 1:
+            df = pd.DataFrame(self.values.reshape((-1, self.shape[-1])))
+            df.columns = self.dimensions
+            df.insert(0, column_id, np.repeat(self.id, self.shape[1]))
+            df.insert(1, column_sort, self.index.flatten())
+            return df
+        # Attribute collection
+        else:
+            df = pd.DataFrame(self.values.reshape((-1, self.shape[-1])))
+            df.columns = self.dimensions
+            df.insert(0, column_id, np.repeat(self.id, self.shape[1]))
+            return df
+
+    @classmethod
+    def from_dataframe(cls, df, column_id='id', column_sort='time'):
+        """
+        Create a collection from a DataFrame.
+
+        The given DataFrame can either contain time series data or 
+        attribute-value data. For compatibility and ease-of-use, we follow the 
+        `tsfresh time  series format (option 1) <https://tsfresh.readthedocs.io/en/latest/text/data_formats.html#input-option-1-flat-dataframe>`_.
+        
+        For time series data, the following format is expected:
+
+        +-----+------+----------+----------+----------+
+        | id  | time | x        | y        | z        |
+        +=====+======+==========+==========+==========+
+        | A   | t1   | x(A, t1) | y(A, t1) | z(A, t1) |
+        +-----+------+----------+----------+----------+
+        | A   | t2   | x(A, t2) | y(A, t2) | z(A, t2) |
+        +-----+------+----------+----------+----------+
+        | ... | ...  | ...      | ...      | ...      |
+        +-----+------+----------+----------+----------+
+        | B   | t1   | x(B, t1) | y(B, t1) | z(B, t1) |
+        +-----+------+----------+----------+----------+
+        | B   | t2   | x(B, t2) | y(B, t2) | z(B, t2) |
+        +-----+------+----------+----------+----------+
+        | ... | ...  | ...      | ...      | ...      |
+        +-----+------+----------+----------+----------+
+
+        For attribute-value data, the following format is expected:
+
+        +-----+------+------+------+
+        | id  | x    | y    | z    |
+        +=====+======+======+======+
+        | A   | x(A) | y(A) | z(A) |
+        +-----+------+------+------+
+        | B   | x(B) | y(B) | z(B) |
+        +-----+------+------+------+
+        | ... | ...  | ...  | ...  |
+        +-----+------+------+------+
+
+        Both time series data and attribute-value data can consist of multiple 
+        variables (**x**, **y**, **z**) that have values for multiple instances
+        (**id**: A, B, ...). For time series data, each variable represents a 
+        time series and hence has multiple values (**time**: t1, t2, ...). The 
+        values and the length of the time column do not have to be identical 
+        across instances.
+
+        Parameters
+        ----------
+        df : DataFrame
+            Time series data or attribute-value given as a DataFrame in the 
+            format specified above.
+        column_id : str, optional
+            Name of the **id** column. Default: 'id'
+        column_sort : str, optional
+            Name of the **time** column. Default: 'time'
+        """
+        # Sort by column_sort, if this column exists
+        if column_sort in df.columns: df = df.sort_values(column_sort)
+        # Group by column_id and gather some information about the data
+        gb = df.groupby(column_id)
+        lengths = list(len(gb.groups[g]) for g in gb.groups)
+        fixed_length = len(set(lengths)) == 1
+        columns = [c for c in df.columns if (c != column_id) and (c != column_sort)]
+        dtype = np.find_common_type(list(df.loc[:, columns].dtypes), [])
+        # Check if attribute-value data
+        if (column_sort not in df.columns) and fixed_length and (lengths[0] == 1):
+            shape = (len(gb.groups), 1, len(columns))
+            values = np.empty(shape, dtype=dtype)
+            id = []
+            for i, g in enumerate(gb.groups):
+                values[i, :, :] = df.loc[gb.groups[g], columns]
+                id.append(g)
+            return Collection(values, id=id, dimensions=columns)
+        # Check if time series data data
+        elif column_sort in df.columns:
+            itype = df.dtypes[column_sort]
+            # Fixed length time series data: create one collection
+            if fixed_length:
+                shape = (len(gb.groups), lengths[0], len(columns))
+                values = np.empty(shape, dtype=dtype)
+                index = np.empty(shape[:2], dtype=itype)
+                id = []
+                for i, g in enumerate(gb.groups):
+                    values[i, :, :] = df.loc[gb.groups[g], columns]
+                    index[i, :] = df.loc[gb.groups[g], column_sort]
+                    id.append(g)
+                return Collection(values, index=index, id=id, dimensions=columns)
+            # Variable length time series data: create multiple collections
+            else:
+                collections = []
+                for i, g in enumerate(gb.groups):
+                    shape = (1, lengths[i], len(columns))
+                    values = np.empty(shape, dtype=dtype)
+                    index = np.empty(shape[:2], dtype=itype)
+                    values[0, :, :] = df.loc[gb.groups[g], columns]
+                    index[0, :] = df.loc[gb.groups[g], column_sort]
+                    c = Collection(values, index=index, id=[g], dimensions=columns)
+                    collections.append(c)
+                return Collection(collections)
+        else:
+            # TODO: Raise an error
+            pass
+
     def __len__(self):
         return self.shape[0]
 
 
 def plot(X, i=0):
-    """Plot time series collections.
+    """
+    Plot time series collections.
 
     Parameters
     ----------
@@ -297,7 +556,7 @@ class Type(Enum):
     """
 
 
-def _reshape(values, index, dimensions):
+def _reshape(values, index, id, dimensions):
     if len(values.shape) == 0:
         v = np.empty((1, 1, 1), dtype=values.dtype)
         v[0, 0, 0] = values
@@ -306,6 +565,11 @@ def _reshape(values, index, dimensions):
         else:
             n = np.empty((1,))
             n[0] = dimensions
+        if id is None:
+            d = np.zeros((1,))
+        else:
+            d = np.empty((1,))
+            d[0] = id
         if index is None:
             i = np.zeros((1, 1))
         else:
@@ -318,6 +582,10 @@ def _reshape(values, index, dimensions):
             n = np.arange(v.shape[2])
         else:
             n = dimensions
+        if id is None:
+            d = np.zeros((1,))
+        else:
+            d = id
         if index is None:
             i = np.zeros((1, 1))
         else:
@@ -330,6 +598,10 @@ def _reshape(values, index, dimensions):
             n = np.arange(v.shape[2])
         else:
             n = dimensions
+        if id is None:
+            d = np.zeros((1,))
+        else:
+            d = id
         if index is None:
             i = np.empty((1, v.shape[1]))
             i[0, :] = np.arange(v.shape[1])
@@ -343,14 +615,18 @@ def _reshape(values, index, dimensions):
             n = np.arange(v.shape[2])
         else:
             n = dimensions
+        if id is None:
+            d = np.arange(values.shape[0])
+        else:
+            d = id
         if index is None:
             t = np.prod(v.shape[:2])
-            i = np.empty((v.shape[0], v.shape[1]))
+            i = np.empty((v.shape[0], v.shape[1]), dtype=int)
             i[:, :] = np.arange(t).reshape(*v.shape[:2][::-1]).T
         else:
             i = np.empty((v.shape[0], v.shape[1]), dtype=index.dtype)
             i[:, :] = index
-    return v, i, n
+    return v, i, d, n
 
 
 def _collections(values):
