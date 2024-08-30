1be093e3c37f7cf5e2643d7ed253b7279b8d2d41
Arne De Brabandere <arne.de.brabandere@telenet.be>
2024-08-30 17:21:10 +0200

    Remove attribute generation

--- tsfuse/construction/algorithm.py
+++ tsfuse/construction/algorithm.py
@@ -25,12 +25,9 @@ class TSFuseExtractor(object):
         transformers="full",
         max_depth=3,
         series_fusion=True,
-        attribute_fusion=True,
         max_series_permutations=10,
-        max_attribute_permutations=1000,
         max_series_correlation=0.9,
         coefficients=0.1,
-        interaction=0.1,
         compatible=lambda x, y: True,
         task="auto",
         verbose=False,
@@ -39,12 +36,9 @@ class TSFuseExtractor(object):
         self.transformers = create(transformers)
         self.max_depth = max_depth
         self.series_fusion = series_fusion
-        self.attribute_fusion = attribute_fusion
         self.max_series_permutations = max_series_permutations
-        self.max_attribute_permutations = max_attribute_permutations
         self.max_series_correlation = max_series_correlation
         self.coefficients = coefficients
-        self.interaction = interaction
         self.compatible = compatible
         self.task = task
         self.verbose = verbose
@@ -61,7 +55,6 @@ class TSFuseExtractor(object):
             warnings.simplefilter("ignore")
             self.series_to_series(data)
             self.series_to_attributes(data)
-            self.attributes_to_attributes(data, y)
             self.select_attributes(data, y)
         for n in self.selected_attributes_:
             n = self.graph_.add_node(n)
@@ -76,7 +69,6 @@ class TSFuseExtractor(object):
             warnings.simplefilter("ignore")
             self.series_to_series(data)
             self.series_to_attributes(data)
-            self.attributes_to_attributes(data, y)
             self.select_attributes(data, y)
         output = dict()
         for n in self.selected_attributes_:
@@ -248,70 +240,9 @@ class TSFuseExtractor(object):
         # Done
         self.extracted_attributes_ = extracted
 
-    def attributes_to_attributes(self, data, y):
-        # Start from extracted attributes
-        attributes = self.extracted_attributes_[:]
-        self.generated_attributes_ = []
-        # Generate attributes for increasing depths
-        if self.attribute_fusion:
-            transformers = self.transformers["attribute-to-attribute-nofusion"]
-            transformers += self.transformers["attribute-to-attribute-fusion"]
-            for depth in range(1, self.max_depth):
-                # Init
-                generated = []
-                nodes_c = [
-                    n for n in attributes if self.depth_[n.trace] < depth
-                ]
-                nodes_d = [
-                    n for n in attributes if self.depth_[n.trace] == depth
-                ]
-                # Generate nodes for all transformers
-                p_prev = 0
-                for t in sorted(transformers, key=self.num_parents):
-                    p = self.num_parents(t)
-                    if p != p_prev:
-                        # Generate compatible permutations
-                        permutations = []
-                        for permutation in self.generate_permutations(
-                            nodes_c, nodes_d, p
-                        ):
-                            if (p > 1) and (
-                                len(permutations)
-                                >= self.max_attribute_permutations
-                            ):
-                                break
-                            if self.check_compatible(data, permutation):
-                                permutations.append(permutation)
-                        # Select interacting permutations
-                        permutations = self.select_interactions(
-                            data, y, permutations
-                        )
-                    # Create transformer node for each permutation
-                    for permutation in permutations:
-                        transformer = t(*permutation)
-                        # Check preconditions
-                        try:
-                            transformer.check_preconditions(
-                                *[data[n.trace] for n in permutation]
-                            )
-                        except InvalidPreconditionError:
-                            continue
-                        # Compute output
-                        output = transformer.transform(
-                            *[data[n.trace] for n in permutation]
-                        )
-                        # Checked, now add the transformer to the generated nodes
-                        if output is not None:
-                            generated.append(transformer)
-                            data[transformer.trace] = output
-                            self.depth_[transformer.trace] = depth + 1
-                # Done for depth
-                attributes += generated
-                self.generated_attributes_ += generated
-
     def select_attributes(self, data, y):
         # Collect all extracted and generated attributes
-        attributes = self.extracted_attributes_ + self.generated_attributes_
+        attributes = self.extracted_attributes_
         # Group similar features
 
         def p(node):
@@ -441,50 +372,6 @@ class TSFuseExtractor(object):
 
         # Done
         return selected
-
-    def select_interactions(self, data, y, permutations):
-        from sklearn_gbmi import h
-
-        # Collect data for the nodes in the permutations only
-        permutations_nodes = set()
-        for permutation in permutations:
-            for node in permutation:
-                permutations_nodes.add(node)
-        # Convert to DataFrame and drop columns with inf/-inf/nan values
-        if len(permutations_nodes) == 0:
-            return []
-        data_permutations = to_dataframe(
-            {n: data[n.trace] for n in permutations_nodes}
-        )
-        data_permutations = data_permutations.replace(
-            [np.inf, -np.inf], np.nan
-        ).dropna(axis=1)
-        data_permutations = data_permutations.astype("float32")
-        data_permutations = data_permutations.loc[
-            :, np.isfinite(data_permutations.values).all(axis=0)
-        ]
-        if data_permutations.shape[1] == 0:
-            return []
-        # Fit a gradient boosted tree model using sklearn
-        if self.task_ == "classification":
-            gbr = GradientBoostingClassifier(
-                random_state=self.random_state
-            ).fit(data_permutations, y)
-        else:
-            gbr = GradientBoostingRegressor(random_state=self.random_state).fit(
-                data_permutations, y
-            )
-        # Compute H-values
-        hvalues = pd.Series(index=[str(p) for p in permutations])
-        for permutation in permutations:
-            if all(str(n) in data_permutations.columns for n in permutation):
-                hvalue = h(
-                    gbr, data_permutations.loc[:, [str(n) for n in permutation]]
-                )
-                hvalues.loc[str(permutation)] = hvalue
-        # Select permutations with large enough H-values
-        selected = hvalues.loc[hvalues > self.interaction]
-        return [p for p in permutations if str(p) in selected.index]
 
     def detect_task(self, y):
         if np.issubdtype(y, np.float64):
